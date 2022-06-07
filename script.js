6e7f47bae46dd18f13f31d2848241a9fdc9f7146
Stuart Theodore <stuart@stuarts-air.lan>
2022-06-08 00:33:57 +0800

    Added crime graph prototype

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ fetch('https://data.gov.sg/api/action/datastore_search?resource_id=83c21090-bd19
         .style("color", d => d.color);
     })
 
-    .then(graph => {
+    .then(exampleGraph => {
         let data = [
             {cx: 400, cy: 600, r:150, fill: "green", stroke: "black"},
             {cx: 100, cy: 200, r:50, fill: "blue", stroke: "purple"},
@@ -100,10 +100,114 @@ fetch('https://data.gov.sg/api/action/datastore_search?resource_id=83c21090-bd19
             .on("mouseover", (event, d) => {
                 d3.select(event.currentTarget)
                 .attr("stroke", "black")
-                .attr("stroke-width", 2);
+                .attr("stroke-width", 1);
             }) 
             .on("mouseout", (event, d) => {
                 d3.select(event.currentTarget)
                 .attr("stroke", "none");
             })
+    })
+
+    .then(graph => {
+        let selectedYear = "2020";
+        let data = crimeData.records.filter(({year}) => year === selectedYear);
+        data.sort((a, b)  => b.value - a.value)
+        console.log(data);
+
+        let margin = {top: 25, right: 25, bottom: 100, left: 75},
+            width = 700 - margin.left - margin.right,
+            height = 350 - margin.top - margin.bottom;
+
+        let chart = d3.select("#crimeGraph")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // Add scales
+        let xScale = d3.scaleBand()
+            .domain(data.map(d => d.level_2))
+            .rangeRound([0, width])
+            .padding(0.1);
+        
+        let yMax = Math.ceil(
+            d3.max(data, d => parseInt(d.value))/1000
+        ) * 1000;
+        
+        let yScale = d3.scaleLinear()
+            .domain([0, yMax])
+            .rangeRound([height, 0]);
+        
+        // Add x-axis
+        chart
+            .append("g")
+            .attr("class", "axis axis-x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale))
+            .selectAll("text")
+                .attr("transform", "rotate(-30)")
+                .style("text-anchor", "end")
+                .style("font-size","10px");
+
+        // Add y-axis
+        chart
+            .append("g")
+            .attr("class", "axis axis-y")
+            .call(d3.axisLeft(yScale).ticks(10))
+            .selectAll("text")
+                .style("font-size","10px");
+        
+        // Add y-axis gridlines
+        // chart.append("g")
+        //     .attr("class", "grid")
+        //     .call(d3.axisLeft(yScale)
+        //         .tickSize(-width)
+        //         .tickFormat('')
+        //         .ticks(10)
+        //     );
+
+        var Tooltip = d3.select("#crimeGraph")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px")
+
+        var mouseover = function(d) {
+            Tooltip
+                .style("opacity", 1)
+            d3.select(this)
+                .style("stroke", "black")
+                .style("opacity", 0.8)
+        }
+
+        var mousemove = function(d) {
+            Tooltip
+                .html("The exact value of<br>this cell is: " + d.value)
+                .style("left", (d3.pointer(this)[0]+70) + "px")
+                .style("top", (d3.pointer(this)[1]) + "px")
+        }
+
+        var mouseout = function(d) {
+            Tooltip
+                .style("opacity", 0)
+            d3.select(this)
+                .style("stroke", "none")
+                .style("opacity", 1)
+        }
+
+        chart.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+                .attr("x", function(d) { return xScale(d.level_2); })
+                .attr("y", function(d) { return yScale(d.value); })
+                .attr("width", function(d) {return xScale.bandwidth(); })
+                .attr("height", function(d) { return height - yScale(d.value); })
+                .attr("class", "svgRect")
+                .attr("fill", "crimson")
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseout", mouseout);
     })
